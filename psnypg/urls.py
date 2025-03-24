8a61172ba1bd7733d143ff1da1e6ec1916f6d624
Godwin Umosen <umosengodwin568@gmail.com>
2025-03-24 16:58:07 +0100

    new psn is been created for branch pharm

--- psnypg/urls.py
+++ psnypg/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Make root URL go to index
-    path('index/', views.index, name='index'),
     # other URL patterns...
 ]
