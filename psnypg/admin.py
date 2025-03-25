9487f44dbe8026cb9c62c674ce431cf7f8f10726
Godwin Umosen <umosengodwin568@gmail.com>
2025-03-25 16:50:55 +0100

    new psn is been created for branch pharm

--- psnypg/admin.py
+++ psnypg/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
 # Register your models here.
+from . import models
+from .models import ExcosPicturePost
+
+#The DeusMagnus main post model admin
+class ExcosPicturePostModelAdmin (admin.ModelAdmin):
+    list_display = ['excos_img','excos_author']
+admin.site.register(ExcosPicturePost, ExcosPicturePostModelAdmin)
--- psnypg/models.py
+++ psnypg/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+from datetime import datetime, date
+
+
+
+# The Executive Model category
+class ExcosPicturePost(models.Model):
+    excos_img = models.ImageField(upload_to='images/')
+    excos_publish_date = models.DateTimeField (auto_now_add= True)
+    excos_author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering =['excos_publish_date']
+    
+    def __str__(self):
+        return self.excos_author 
+    
+    def get_absolute_url(self):
+        return reverse('index',)
